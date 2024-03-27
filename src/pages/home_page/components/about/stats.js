066ff2f58ae1281e01cbe24f72a620c2850a69a1
developer <85758481+narzullayev0772@users.noreply.github.com>
2024-03-27 16:29:33 +0500

    initial

--- src/pages/home_page/components/about/stats.js
+++ src/pages/home_page/components/about/stats.js
@@ -1,45 +1,47 @@
 import React from "react";
-import { Box, Typography } from "@mui/joy";
-import { stats } from "../../../../utils/stats";
+import {Box, Typography} from "@mui/joy";
+import {stats} from "../../../../utils/stats";
 import CountUp from "react-countup";
+
 const Stats = () => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        padding: "20px 0",
-      }}
-    >
-      {stats.map((e, i) => {
-        return (
-          <Box
-            key={i}
+    return (
+        <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "10px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                padding: "20px 0",
+                gap: "10px"
             }}
-          >
-            <Typography
-              level="h2"
-              fontWeight={"500"}
-            >
-              <CountUp
-                end={e.value}
-                duration={5}
-                suffix="+"
-                separator=" "
-                scrollSpyOnce={true}
-              />
-            </Typography>
-            <Typography>{e.label}</Typography>
-          </Box>
-        );
-      })}
-    </Box>
-  );
+        >
+            {stats.map((e, i) => {
+                return (
+                    <Box
+                        key={i}
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "10px",
+                        }}
+                    >
+                        <Typography
+                            level="h2"
+                            fontWeight={"500"}
+                        >
+                            <CountUp
+                                end={e.value}
+                                duration={5}
+                                suffix="+"
+                                separator=" "
+                                scrollSpyOnce={true}
+                            />
+                        </Typography>
+                        <Typography>{e.label}</Typography>
+                    </Box>
+                );
+            })}
+        </Box>
+    );
 };
 
 export default Stats;
